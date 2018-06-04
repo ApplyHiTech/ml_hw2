06115ba77e7cba68dffff67509b0b56c0c0cfe96
Apply Hi Tech <dar2144@columbia.edu>
2018-06-04 16:55:50 +0300

    started to expand the number of test cases
    started to play with the outcome possibilities

--- ex2.py
+++ ex2.py
@@ -13,30 +13,15 @@ GHOSTS = ["green", "red", "blue", "yellow"]
 BLOCKED_DOTS = [21, 31, 41, 51, 71]
 
 
-def count_dots(state):
-    l = state.values()
-    # print(state)
-    # print(l)
-    j = 0
-    for i in l:
-        if i == 11:
-            j += 1
-
-    return j
-
-
 class PacmanController(mdp.MDP):
     """This class is a controller for a pacman agent."""
-
 
     def __init__(self, problem, steps):
         self.original_problem = deepcopy(problem)
         start_state, special_things = checker.problem_to_state(problem)
         self.steps = steps
         self.current_state_of_board, self.current_special_things = checker.problem_to_state(problem)
-        self.accumulated_reward = 0
         self.eval = checker.Evaluator(0, problem, steps)
-
         self.act_list = ACT_LIST
         all_states, trans_dict, rewards = self.compute_states
         mdp.MDP.__init__(self,init=start_state, actlist=["U", "D", "R", "L"], terminals=[], transitions=trans_dict, states=all_states, )
@@ -51,28 +36,21 @@ class PacmanController(mdp.MDP):
         print("end of initialization\n\n\n\n")
         return
 
-    def no_dots(self,eval_state):
-        for k,v in eval_state.items():
-            if v%10-1==0:
-                return False
 
-        return True
-
-
-
-    def eval_state_to_ab_state_plus_md(self,eval):
+    def eval_state_to_ab_state_plus_md(self,eval): #
         # we assume that if pacman is dead then we return a relaxed version of the board
         min_md = 2**32-1
-        if self.no_dots(eval.state):
-            min_md = 0
+
         relaxed_eval = deepcopy(eval)
         # make sure pacman exists
         if "pacman" in eval.special_things and eval.special_things["pacman"] is not 'dead':
             pacman_place = eval.special_things["pacman"]
         # iterate over all possible ghosts
+        num_ghosts = 0
         for color in GHOSTS:
             # make sure ghost exists on board
             if color in eval.special_things.keys():
+                num_ghosts+=1
                 cur_ghost_place = eval.special_things[color]
                 if 'pacman_place' in locals():
                     min_md = min(min_md,abs(pacman_place[0] - cur_ghost_place[0]) + abs(pacman_place[1] - cur_ghost_place[1]))
@@ -80,9 +58,13 @@ class PacmanController(mdp.MDP):
 
                 relaxed_eval.state[eval.special_things[color]] = 10 + (relaxed_eval.state[eval.special_things[color]] - checker.COLOR_CODES[color])
 
+
         # relax poison dots:
+        num_pills = 0
         if "poison" in eval.special_things.keys():
             for cell in eval.special_things["poison"]:
+                num_pills+=1
+
                 if eval.state[(cell[0], cell[1])] == 71:
                     relaxed_eval.state[(cell[0], cell[1])] = 11
                 else:
@@ -90,30 +72,33 @@ class PacmanController(mdp.MDP):
 
 
 
-        return (checker.Evaluator.state_to_agent(relaxed_eval), min_md)
+
+        return (checker.Evaluator.state_to_agent(relaxed_eval), min_md, num_pills, num_ghosts)
 
     @property
     def compute_states(self):
-        j = 0
         # BFS
         a_eval = deepcopy(self.eval)
         frontier = FIFOQueue()
+        min_front_reward = 0
         frontier.append(a_eval)  # Eval
         explored = set()  # ABSTRACT ( Eval.state ) + MD
         possible_state = set() # we return this so we don't have any keyerror.
         T = {}
         R = {}
         start_time = time.time()
-        while frontier and j < 10000 and time.time()-start_time <40 :
-            #j is just a counter to make sure we avoid infinite loop.
-            j += 1
+        while frontier and time.time() - start_time < 40 :
             temp_eval = frontier.pop()
             explored.add(self.eval_state_to_ab_state_plus_md(temp_eval)) # add state.
 
             # explore possible children of the board if they exist.
 
             if "pacman" in temp_eval.special_things and temp_eval.special_things["pacman"] is not 'dead':
-                before_action_reward = temp_eval.accumulated_reward
+                before_action_h_value = self.set_h(temp_eval)
+                #print(temp_eval.special_things)
+
+                #print(before_action_h_value)
+
                 parent_state_md = self.eval_state_to_ab_state_plus_md(temp_eval)
 
                 # children
@@ -128,7 +113,9 @@ class PacmanController(mdp.MDP):
                     after_action_reward = child_eval.accumulated_reward
 
                     # Did the action finish the board?
-                    if after_action_reward - before_action_reward  >= 30:
+                    if checker.Evaluator.finished_the_game(child_eval):
+
+                    #if after_action_reward - before_action_h_value  >= 30:
                         #print('SOMETHING STRANGE')
                         empty_state = deepcopy(temp_eval)
                         if action == "R":
@@ -166,9 +153,10 @@ class PacmanController(mdp.MDP):
 
                         explored.add(self.eval_state_to_ab_state_plus_md(empty_state))
                         possible_state.add(self.eval_state_to_ab_state_plus_md(empty_state))
+
                     elif child_eval.special_things["pacman"] is not 'dead':
 
-                        R[next_state_md] = self.set_reward(temp_eval.accumulated_reward,parent_state_md[1])
+                        R[next_state_md] = self.set_h(temp_eval)
 
                         T[(parent_state_md, action)] = (1, next_state_md)
                         possible_state.add(next_state_md)
@@ -176,27 +164,40 @@ class PacmanController(mdp.MDP):
                     # we dont want to add states where pacman is dead:
                     if "pacman" in child_eval.special_things and child_eval.special_things["pacman"] is not 'dead':
                         if next_state_md not in explored and child_eval not in frontier:
-                            frontier.append(child_eval)
+                            if child_eval.accumulated_reward >= min_front_reward-1:
+                                min_front_reward = max(child_eval.accumulated_reward,min_front_reward)
+                                frontier.append(child_eval)
+                                print("Min Reward %s" % min_front_reward)
+
 
             print("Finished loop: States: "+ str(len(explored)) +" Queue: " + str(len(frontier)))
         print("SIZE OF EXPLORED = %s SIZE OF T = %s SIZE OF R = %s" % (len(explored),len(T),len(R)))
         return possible_state, T, R
 
-    def set_reward(self,accumulated_reward, manhattan_distance_to_ghost):
-        if manhattan_distance_to_ghost < 2:
-            #
-            return accumulated_reward
-        elif manhattan_distance_to_ghost < 4:
-            #neutral
-            return accumulated_reward + 0.5
-        elif manhattan_distance_to_ghost >= 4:
-            return accumulated_reward + 1
+    def numb_of_ghosts(self):
+        i =0
+        for k in self.current_special_things:
+            if k in checker.LOSS_INDEXES:
+                i+=1
+
+        return i
+
+    def set_h(self,temp_eval): # heuristic
+        accum_reward = temp_eval.accumulated_reward
+
+        state, min_dist, num_pills, num_ghosts = self.eval_state_to_ab_state_plus_md(temp_eval)
+        #print("acc reward %s" % accum_reward)
+        #print("num ghosts %s"   % num_ghosts)
+        #print("num pills %s " % num_pills)
+        #print("min_dist %s " % min_dist)
+
+        return 10*accum_reward +1*min_dist + 50*num_pills + 5*num_ghosts
 
     def T(self, state, action):
         if (state,action) in self.transitions:
             return [self.transitions[(state, action)]]
         else:
-            print("Not found T")
+            #print("Not found T")
             return [(1,state)]
     def R(self, state):
         if state in self.reward:
